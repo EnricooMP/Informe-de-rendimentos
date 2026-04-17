ef7807e4e3d5c73ab0343da8d417635a04762b58
EnricooMP <palopolienrico@gmail.com>
2026-04-17 10:32:54 -0300

    main code

---
+++ main.py
@@ -0,0 +1,26 @@
+while True:
+    try:
+        v1 = float(input("\nInforme os rendimentos de 2022: "))
+        v2 = float(input("\nInforme os rendimentos de 2023: "))
+    except ValueError:
+        print("Erro: insira apenas numeros!")
+        continue
+
+    variação_percentual = ((v2 - v1) / v1) * 100
+
+    if variação_percentual >= 20:
+        print(f"\nO rendimento foi de {variação_percentual:.1f}%, Bonificação total")
+    elif 2 <= variação_percentual <= 20:
+        print(f"\nO rendimento foi de {variação_percentual:.1f}%, Pequena bonificação")
+    elif -10 <= variação_percentual <= 2:
+         print(f"\nO rendimento foi de {variação_percentual:.1f}%, Planejamento de politicas de incentivo de vendas")
+    elif variação_percentual <= -10:
+        print(f"\nO rendimento foi de {variação_percentual:.1f}%, Corte de gastos")
+    else:
+        print("Valor inválido")
+    
+    pergunta = input("\nDeseja informar outra variação de rendimento? (s/n): ").lower().strip()
+    if pergunta != 's':
+        print("Encerrando relatório. Até logo!")
+        break
+       
